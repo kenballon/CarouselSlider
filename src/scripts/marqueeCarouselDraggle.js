3d95b58a566a49861854b3fce13db9c93c57dd88
Kenneth Ballon <49665487+kenballon@users.noreply.github.com>
2023-08-29 16:40:17 +0800

    Update marqueeCarouselDraggle.js
    added class info

--- src/scripts/marqueeCarouselDraggle.js
+++ src/scripts/marqueeCarouselDraggle.js
@@ -1,3 +1,12 @@
+/**
+ * Marquee Class.
+ * This class is for Infinite Marquee Effect. Call this class when we want a maquee effect.
+ * It expect the below parameters for it to work.
+ *
+ * @param {string} marqueeWrapper - CSS class of the parent container wrapper of marqueeContent.
+ * @param {string} marqueeContent - CSS class of the parent of each marquee child. Let's say cards or images.
+ */
+
 export default class Marquee {
   constructor(marqueeWrapper, marqueeContent) {
     this.marqueeWrapper = marqueeWrapper;
@@ -6,9 +15,6 @@ export default class Marquee {
     this.isDragging = false;
     this.startX = 0;
     this.currentX = 0;
-
-    const cloneFirstChild = this.marqueeContent.cloneNode(true);
-    this.marqueeWrapper.appendChild(cloneFirstChild);
 
     this.marqueeWrapper.addEventListener(
       "mousedown",
@@ -20,6 +26,24 @@ export default class Marquee {
       this.marqueeWrapper.classList.remove("is-grabbing");
     });
     this.marqueeWrapper.addEventListener("mouseleave", this.endDrag.bind(this));
+
+    // Touch events for mobile and tablets
+    this.marqueeWrapper.addEventListener(
+      "touchstart",
+      this.startDragTouch.bind(this)
+    );
+    this.marqueeWrapper.addEventListener(
+      "touchmove",
+      this.dragTouch.bind(this)
+    );
+    this.marqueeWrapper.addEventListener(
+      "touchend",
+      this.endDragTouch.bind(this)
+    );
+    this.marqueeWrapper.addEventListener(
+      "touchcancel",
+      this.endDragTouch.bind(this)
+    );
   }
 
   startDrag(e) {
@@ -39,6 +63,29 @@ export default class Marquee {
   }
 
   endDrag() {
+    this.isDragging = false;
+    this.marqueeWrapper.style.animationPlayState = "running";
+    this.marqueeWrapper.classList.remove("is-dragging");
+  }
+
+  // Touch event handlers
+  startDragTouch(e) {
+    this.isDragging = true;
+    this.startX = e.touches[0].clientX - this.currentX;
+    this.marqueeWrapper.classList.add("is-grabbing");
+  }
+
+  dragTouch(e) {
+    if (this.isDragging) {
+      const newX = e.touches[0].clientX - this.startX;
+      this.marqueeWrapper.style.animationPlayState = "paused";
+      this.marqueeWrapper.scrollLeft -= newX - this.currentX;
+      this.currentX = newX;
+      this.marqueeWrapper.classList.add("is-dragging");
+    }
+  }
+
+  endDragTouch() {
     this.isDragging = false;
     this.marqueeWrapper.style.animationPlayState = "running";
     this.marqueeWrapper.classList.remove("is-dragging");
